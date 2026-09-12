326c82d33a4b9896639a522402840603cd77f439
ODUZZ <tobiloba428@gmail.com>
2026-09-12 21:20:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuButton = document.getElementById("menuButton");
+const navbar = document.getElementById("navbar");
+
+
+menuButton.addEventListener("click", () => {
+
+    navbar.classList.toggle("show");
+
+});
